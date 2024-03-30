16be7d28e464a52a36d7ae6a2981455bb947bf86
spacenavy90 <10876259+spacenavy90@users.noreply.github.com>
2024-03-29 21:07:53 -0500

    Republic Arquitens

--- armada-legacy-customdb.lua
+++ armada-legacy-customdb.lua
@@ -16,157 +16,42 @@
 --
 -- Definitions merge multiple tables to reduce duplication of common properties:
 -- Examples of Card, Ship, Squadron definitions below.
-ASSETS_ROOT = 'https://raw.githubusercontent.com/gamerkhang/Armada-Legacy-TTSIncludeDir/master/assets/'
+
+ASSETS_ROOT = 'https://raw.githubusercontent.com/spacenavy90/ArmadaLegacyTTS-IncludeDir/master/assets/'
+
 function onload()
-	printToAll("Loading Armada Legacy collection by SpaceNavy",{1,0.5,0})
+	printToAll("Loading Armada Legacy collection",{1,0.5,0})
 	
 	REPUBLIC_SHIP = {
 		back = "https://i.imgur.com/8s63Ngl.png",
         faction = "Republic"
     }
 	Ship:new(SmallShip,REPUBLIC_SHIP, {
-	name = "Arquitens-class Light Cruiser",
-         front = "https://i.imgur.com/vMgbQB3.png",
-         mesh = "http://paste.ee/p/XZLIh",
-         diffuse = "http://i.imgur.com/2A2pAEI.png",
-         ruler = "http://paste.ee/r/FSip2", 
-         maneuver = {{"II"},{"-","II"},{"-","-","II"}},
-         defense_tokens = {DEF_EVADE, DEF_REDIRECT, DEF_CONTAIN, DEF_SALVO},
-         shields = {2,2,2,2}, -- Front, left, right, rear
-         cost = 58,
-         aliases = {},
-         command = 2 
-	
---EXAMPLES:
-    
-	REPUBLIC_SQUAD = {
-        back = "https://i.imgur.com/8s63Ngl.png",
-        faction = "Republic"
-    }
-    NABOO = {
-        mesh = "https://paste.ee/r/860Wh",
-        diffuse = "https://i.imgur.com/wqDzbHL.png",
-        health = 4,
-        move = 5,
-        defense_tokens = {}}
-    Squadron:new(NABOO,REPUBLIC_SQUAD, {
-        name = "Naboo Royal N-1 Starfighter",
-        front = "https://i.imgur.com/oEEJn4f.jpeg",
-        cost = 11, --TODO: Proper cost?
-        aliases = {"N-1 Starfighter","N-1"}
+	name = "Arquitens-class Command Cruiser",
+        front = ASSETS_ROOT.."ships/republic/rep_arq/reparq_command_card.png",
+        mesh = ASSETS_ROOT.."ships/republic/rep_arq/mesh.obj",
+        diffuse = ASSETS_ROOT.."ships/republic/rep_arq/reparq_command_texture.png",
+        ruler = "http://paste.ee/r/FSip2", 
+        maneuver = {{"II"},{"-","II"},{"-","-","II"}},
+        defense_tokens = {DEF_EVADE, DEF_REDIRECT, DEF_CONTAIN, DEF_SALVO},
+        shields = {2,2,2,2}, -- Front, left, right, rear
+        cost = 60,
+        aliases = {},
+        command = 2
     })
-	
-	REBEL_SQUAD = {
-        back = "https://i.imgur.com/8s63Ngl.png", --card back
-        faction = "Rebellion"
-    }
-
-    ship = {
-        mesh = "http://paste.ee/r/ZqCC6",
-        diffuse = "http://i.imgur.com/QSLaqgW.png", --model texture
-        health = 5,
-        move = 2,
-        defense_tokens = {}}
-
-    Squadron:new(ship, REBEL_SQUAD, {
-	    name = "Keyan Farlander",
-        front = "https://i.imgur.com/r4bx4xg.png", --card front
-        diffuse = "http://i.imgur.com/r7YB80F.png", --model texture
-        defense_tokens = {DEF_BRACE,DEF_BRACE},
-        cost = 99 })
-		
-	Squadron:new(ship, REBEL_SQUAD, {
-	    name = "Doxic Halcyon",
-        front = "https://i.imgur.com/r4bx4xg.png", --card front
-        diffuse = "http://i.imgur.com/r7YB80F.png", --model texture
-        defense_tokens = {DEF_BRACE,DEF_BRACE},
-        cost = 420
-    })
-    PurgeCard("Keyan Farlander",20)
-
-
---	Commander = {
---         type="Commander",
---         back = "https://i.imgur.com/fMJMaYo.png" } -- card back
---     Rebel = { factions={"Rebellion"} } -- factions must be a table for cards, since multi faction upgrades exist
---     Card:new(Commander,Rebel,{name="Admiral Ackbar",  front="http://i.imgur.com/HFVv48K.jpg", cost=38})
---
---     REBEL_SHIP = {
---         back = "https://i.imgur.com/vUaQViH.png",  -- card back
---         faction = "Rebellion" }
---     Ship:new(SmallShip,REBEL_SHIP,{
---         name = "GR-75 Medium Transports",
---         front = "https://i.imgur.com/vMgbQB3.png", --Card front image
---         mesh = "http://paste.ee/p/XZLIh",
---         diffuse = "http://i.imgur.com/2A2pAEI.png", -- model texture
---         ruler = "http://paste.ee/r/FSip2", -- custom ruler mesh
---         maneuver = {{"II"},{"I","II"},{"-","I","II"}},
---         defense_tokens = {DEF_SCATTER, DEF_EVADE},
---         shields = {1,1,1,1}, -- Front, left, right, rear
---         cost = 18,
---         aliases = {},
---         command = 1 -- Number of command dials
---     })
---
---     REBEL_SQUAD = {
---         back = "https://i.imgur.com/8s63Ngl.png", --card back
---         faction = "Rebellion"
---     }
---     ship = {
---         mesh = "http://paste.ee/r/ZqCC6",
---         diffuse = "http://i.imgur.com/QSLaqgW.png", --model texture
---         health = 5,
---         move = 2,
---         defense_tokens = {}}
---     Squadron:new(ship, REBEL_SQUAD, {
--- 	       name = "B-wing Squadron",
---         front = "https://i.imgur.com/bB11RGw.png", --card front
---         cost = 14,
---         aliases = {'B-wing Squadrons', "B-wing"}
---     })
---     Squadron:new(ship, REBEL_SQUAD, {
--- 	       name = "Keyan Farlander",
---         front = "https://i.imgur.com/r4bx4xg.png", --card front
---         diffuse = "http://i.imgur.com/r7YB80F.png", --model texture
---         defense_tokens = {DEF_BRACE,DEF_BRACE},
---         cost = 20
---     })
-
-	UpdateCard("Nebulon-B Escort Frigate",57,{
-        cost = 54,
-        maneuver = {{"II"},{"I","I"},{"-","I","II"}},
-        defense_tokens = {DEF_EVADE, DEF_BRACE, DEF_CONTAIN},
-        front = "https://vassalwarlords.twilightpeaks.net/assets/cards/ships/rebel/shipcard_nebulon-b-escort-frigate.png"
-    })
-	
-	Squadron:new(TIE_ADV, {
-        name = "Ved Foslo",
-        front = "https://vassalwarlords.twilightpeaks.net/assets/cards/squadrons/empire/squadcard_ved-foslo.png",
-        diffuse = "http://i.imgur.com/YJm6aoS.png",
-        defense_tokens = {DEF_BRACE,DEF_EVADE},
-        cost = 16
-		
-	TIE_BOM = GetDefinition("TIE Bomber Squadron",9)
-    Squadron:new(TIE_BOM, {
-        name = "Scimitar Squadron",
-        front = "https://vassalwarlords.twilightpeaks.net/assets/cards/squadrons/empire/squadcard_scimitar-squadron.png",
-        cost = 12,
-        aliases = {"Scimitar Squadrons"}
-    })
-    Squadron:new(TIE_BOM, {
-    	name = "Deathfire",
-        front = "https://vassalwarlords.twilightpeaks.net/assets/cards/squadrons/empire/squadcard_deathfire.png",
-        diffuse = "http://i.imgur.com/o9KJLV8.png",
-        defense_tokens = {DEF_BRACE,DEF_EVADE},
-        cost = 14
-    })
-	
-	ION_CANNON = GetDefinition("Point Defense Ion Cannons",4)
-    Card:new(ION_CANNON,{
-        name="Ion Pulse Missiles",
-        cost=5,
-        front="https://vassalwarlords.twilightpeaks.net/assets/cards/upgrades/wx_ion_ion-pulse-missiles.png"
-    })
+    Ship:new(SmallShip,REPUBLIC_SHIP, {
+        name = "Arquitens-class Light Cruiser",
+        front = ASSETS_ROOT.."ships/republic/rep_arq/reparq_light_card.png",
+        mesh = ASSETS_ROOT.."ships/republic/rep_arq/mesh.obj",
+        diffuse = ASSETS_ROOT.."ships/republic/rep_arq/reparq_light_texture.png",
+        ruler = "http://paste.ee/r/FSip2", 
+        maneuver = {{"II"},{"-","II"},{"-","-","II"}},
+        defense_tokens = {DEF_EVADE, DEF_REDIRECT, DEF_CONTAIN, DEF_SALVO},
+        shields = {2,2,2,2}, -- Front, left, right, rear
+        cost = 58,
+        aliases = {},
+        command = 2
+        })
 
 end
 
